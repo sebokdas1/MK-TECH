6d3f03d893020fb136ceb1dda5eb73974b158f04
sebokdas1 <sebok.das.ff@gmail.com>
2022-06-06 19:06:47 +0600

    available

--- src/page/Advantage.js
+++ src/page/Advantage.js
@@ -6,24 +6,25 @@ import './Advantage.css';
 const Advantage = () => {
     return (
         <div className='mt-4'>
-            <h2 className='text-center font-bold'>Candere Advantages</h2>
+            <h1 className='text-center text-3xl available'>AVAILABLE FOR EVERYONE</h1>
+            <h2 className='text-center font-bold mt-2'>Candere Advantages</h2>
             <div className='empty-div-featured mx-auto mt-2'></div>
             <div className='advantage-content mt-4'>
                 <div className='advantage-second-container'>
                     <div>
                         <p className='font-icons shadow-lg'> <FontAwesomeIcon icon={faBullhorn} /></p>
-                        <p className='mt-4'>Discount Announcement</p>
-                        <button>Get Discount</button>
+                        <p className='mt-4 font-bold'>Discount Announcement</p>
+                        <button className=''>Get Discount</button>
                     </div>
                     <div>
                         <p className='font-icons shadow-lg'> <FontAwesomeIcon icon={faPaperPlane} /></p>
-                        <p className='mt-4'>Direct Messages</p>
-                        <button>Chat</button>
+                        <p className='mt-4 font-bold'>Direct Messages</p>
+                        <button className=''>Chat</button>
                     </div>
                     <div>
                         <p className='font-icons shadow-lg'> <FontAwesomeIcon icon={faTruck} /></p>
-                        <p className='mt-4'>Tracking Order Delevery</p>
-                        <button>Track Order</button>
+                        <p className='mt-4 font-bold'>Tracking Order Delevery</p>
+                        <button className=''>Track Order</button>
                     </div>
                 </div>
             </div>
